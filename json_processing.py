e3c63323d0964a0400e35fac4d93dfcd23901b78
dalbodeule <11470513+dalbodeule@users.noreply.github.com>
2024-03-22 20:25:00 +0900

    update with training....

---
+++ json_processing.py
@@ -0,0 +1,29 @@
+from typing import List, Dict
+
+import json
+import pandas as pd
+import os
+
+def read_json_file(file_path: str) -> pd.DataFrame:
+    """
+    JSON 파일을 읽어와 파싱한 데이터를 리스트로 반환하는 함수
+    :param file_path: 읽어올 JSON 파일의 경로
+    :return: JSON 파일을 파싱한 리스트
+    """
+
+    df = pd.DataFrame(columns=['text', 'corrected'])
+
+    # JSON 파일 읽어오기
+    with open(file_path, 'r', encoding='utf-8-sig') as f:
+        data: Dict = json.load(f)
+        df.loc[len(df)] = {'text': data.get('ko'), 'corrected': data.get('corrected')}
+    
+    return df
+
+def get_json_files(folder_path):
+    json_files = []
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith(".json"):
+                json_files.append(os.path.join(root, file))
+    return json_files
