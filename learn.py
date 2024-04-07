12394398c09eef5ef9c3dac14d173b8855bd5bf3
20203153 <123941587+20203153@users.noreply.github.com>
2024-04-07 20:08:51 +0900

    moved m2 max

--- learn.py
+++ learn.py
@@ -1,10 +1,6 @@
 from torch.utils.data import Dataset, DataLoader
-from transformers import BertTokenizerFast, GPT2LMHeadModel
-from tqdm import tqdm
-from sklearn.metrics import f1_score
+from transformers import BertTokenizerFast
 
-import torch
-import pickle
 import pandas as pd
 import torch
 
@@ -12,19 +8,18 @@ LOCATION = '.'
 
 # GPT-3 모델과 토크나이저 불러오기
 tokenizer = BertTokenizerFast.from_pretrained("kykim/gpt3-kor-small_based_on_gpt2")
-max_length = 64
-loader_size = 16
+max_length = 128
 
 # 훈련용 및 검증용 데이터셋 클래스 정의
 class TextDataset(Dataset):
-    def __init__(self, data: pd.DataFrame, tokenizer: BertTokenizerFast, max_length):
+    def __init__(self, data, tokenizer, max_length):
         self.data = data
         self.tokenizer = tokenizer
         self.max_length = max_length
-    
+
     def __len__(self):
         return len(self.data)
-    
+
     def __getitem__(self, idx):
         input_text = str(self.data.iloc[idx]['text'])
         output_text = str(self.data.iloc[idx]['corrected'])
@@ -44,6 +39,16 @@ validate_dataset = TextDataset(validate, tokenizer, max_length)
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 validate_loader = DataLoader(validate_dataset, batch_size=32, shuffle=True)
 
+from transformers import GPT2LMHeadModel, Trainer, TrainingArguments, DataCollatorForLanguageModeling, TrainerCallback
+from tqdm.notebook import tqdm
+from sklearn.metrics import accuracy_score
+from datasets import load_metric
+
+import torch
+import pickle
+import os
+import gc
+
 device = torch.device('cpu')
 
 if torch.backends.mps.is_available():
@@ -51,62 +56,75 @@ if torch.backends.mps.is_available():
 elif torch.cuda.is_available():
     device = torch.device('cuda')
 
-# Model and tokenizer (assuming you have them defined elsewhere)
-model = GPT2LMHeadModel.from_pretrained("kykim/gpt3-kor-small_based_on_gpt2").to(device=device)
-
-all_metrics = []
+model = GPT2LMHeadModel.from_pretrained("kykim/gpt3-kor-small_based_on_gpt2")
 
 model.train()
 
 lr = 2e-5
 loss = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+SAVE_PATH = f"{LOCATION}/all_metrics.pkl"
 
-# 학습 및 검증 함수 정의
-def train(train_loader, model, optimizer):
-    model.train()
-    total_loss = 0.0
-    for batch in tqdm(train_loader):
-        inputs = batch['input_ids'].to(model.device)
-        labels = batch['output_ids'].to(model.device)
-        optimizer.zero_grad()
-        outputs = model(inputs, labels=labels)
-        loss = outputs.loss
-        loss.backward()
-        optimizer.step()
-        total_loss += loss.item()
-    return total_loss / len(train_loader)
+if os.path.exists(SAVE_PATH):
+    with open(SAVE_PATH, "rb") as f:
+        all_metrics = pickle.load(f)
+else:
+    all_metrics = []
 
-def validate(validate_loader, model):
-    model.eval()
-    total_loss = 0.0
-    predictions = []
-    targets = []
-    with torch.no_grad():
-        for batch in tqdm(validate_loader):
-            inputs = batch['input_ids'].to(model.device)
-            labels = batch['output_ids'].to(model.device)
-            outputs = model(inputs, labels=labels)
-            loss = outputs.loss
-            total_loss += loss.item()
-            predictions.extend(outputs.logits.argmax(dim=-1).tolist())
-            targets.extend(labels.tolist())
-    roc_auc = roc_auc_score(targets, predictions)
-    f1 = f1_score(targets, predictions, average='macro')
-    return total_loss / len(validate_loader), roc_auc, f1
+def get_latest_checkpoint(path):
+    checkpoints = [f for f in os.listdir(path) if f.startswith('checkpoint-')]
+    if checkpoints:
+        return os.path.join(path, max(checkpoints))
+    else:
+        return None
 
-# 학습 및 검증
-epochs = 10
-for epoch in tqdm(range(epochs)):
-    train_loss = train(train_loader, model, optimizer)
-    validate_loss, roc_auc, f1 = validate(validate_loader, model)
+def compute_metrics(eval_pred):
+    metric_bleu = load_metric("bleu")
+    predictions, labels = eval_pred
 
-    print(f"Epoch {epoch + 1}/{epochs}, Train Loss: {train_loss}, Validate Loss: {validate_loss}, F1 Score: {f1}")
-    all_metrics.append({
-        'epoch':epoch + 1,
-        'train_loss': train_loss,
-        'val_loss': validate_loss,
-        'f1': f1
-    })
-    with open("{LOCATION}/all_metrics.pkl", "wb") as f:
-        pickle.dump(all_metrics, f)
+    # BART 모델은 디코딩된 토큰을 제공하기 때문에 디코딩하여 문자열로 변환
+    generated_sentences = [pred["generated_text"] for pred in predictions]
+
+    # BLEU 점수 계산
+    bleu_score = metric_bleu.compute(predictions=generated_sentences, references=labels)
+
+    # 정확도 계산을 위해 생성된 문장과 레이블 비교
+    # 여기서는 완전히 일치하는 문장이 있는지 여부를 확인하여 정확도를 계산
+    accuracy = accuracy_score(labels, generated_sentences)
+
+    scores = {"bleu": bleu_score["score"], "accuracy": accuracy}
+    all_metrics.append(scores)
+    with open(SAVE_PATH, "wb") as f:
+        pickle.dump(SAVE_PATH, f)
+    return scores
+
+data_collactor = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+
+training_args = TrainingArguments(
+    output_dir=f"{LOCATION}/model", # 모델 저장 경로
+    overwrite_output_dir=True,  # 기존 모델을 덮어쓰기
+    learning_rate=2e-5,  # 학습률 설정
+    num_train_epochs=100,  # 학습 에포크 설정
+    save_steps=1000,  # 모델 저장 스텝 설정
+    save_total_limit=10,  # 최대 모델 저장 개수 설정
+    warmup_steps=500,  # 워밍업 스텝 설정
+    weight_decay=0.01
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=validate_dataset,
+    data_collator=data_collactor,
+    tokenizer=tokenizer,
+    compute_metrics=compute_metrics
+)
+
+torch.cuda.empty_cache()
+gc.collect()
+
+trainer.train(resume_from_checkpoint = True if get_latest_checkpoint(f'{LOCATION}/model/') else False)
+
+
+trainer.save_model(f"{LOCATION}/model")
