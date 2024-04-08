a36a0184bce491b8acd1afdc90634585371e823d
20203153 <123941587+20203153@users.noreply.github.com>
2024-04-08 12:45:57 +0900

    some changes.

--- learn.py
+++ learn.py
@@ -27,7 +27,7 @@ class TextDataset(Dataset):
         output_ids = self.tokenizer.encode(output_text, max_length=self.max_length, truncation=True, padding='max_length')
         return {
             'input_ids': torch.tensor(input_ids, dtype=torch.long),
-            'output_ids': torch.tensor(output_ids, dtype=torch.long)
+            'labels': torch.tensor(output_ids, dtype=torch.long)
         }
 
 train = pd.read_csv(f'{LOCATION}/train.csv')
@@ -35,9 +35,6 @@ validate = pd.read_csv(f'{LOCATION}/validate.csv')
 
 train_dataset = TextDataset(train, tokenizer, max_length)
 validate_dataset = TextDataset(validate, tokenizer, max_length)
-
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-validate_loader = DataLoader(validate_dataset, batch_size=32, shuffle=True)
 
 from transformers import GPT2LMHeadModel, Trainer, TrainingArguments, DataCollatorForLanguageModeling, TrainerCallback
 from tqdm.notebook import tqdm
@@ -81,35 +78,25 @@ def get_latest_checkpoint(path):
 def compute_metrics(eval_pred):
     metric_bleu = load_metric("bleu")
     predictions, labels = eval_pred
+    decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
 
-    # BART 모델은 디코딩된 토큰을 제공하기 때문에 디코딩하여 문자열로 변환
-    generated_sentences = [pred["generated_text"] for pred in predictions]
+    bleu_score = metric_bleu.compute(predictions=decoded_preds, references=[decoded_labels])
 
-    # BLEU 점수 계산
-    bleu_score = metric_bleu.compute(predictions=generated_sentences, references=labels)
-
-    # 정확도 계산을 위해 생성된 문장과 레이블 비교
-    # 여기서는 완전히 일치하는 문장이 있는지 여부를 확인하여 정확도를 계산
-    accuracy = accuracy_score(labels, generated_sentences)
-
-    scores = {"bleu": bleu_score["score"], "accuracy": accuracy}
-    all_metrics.append(scores)
-    with open(SAVE_PATH, "wb") as f:
-        pickle.dump(SAVE_PATH, f)
-    return scores
+    return {"bleu": bleu_score["score"]}
 
 data_collactor = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
 
 training_args = TrainingArguments(
-    output_dir=f"{LOCATION}/model", # 모델 저장 경로
-    overwrite_output_dir=True,  # 기존 모델을 덮어쓰기
-    learning_rate=2e-5,  # 학습률 설정
-    num_train_epochs=100,  # 학습 에포크 설정
+    output_dir=f"{LOCATION}/model", 
+    overwrite_output_dir=True,  
+    learning_rate=2e-5,  
+    num_train_epochs=5,
     per_device_train_batch_size=16,
     per_device_eval_batch_size=16,
-    save_steps=1000,  # 모델 저장 스텝 설정
-    save_total_limit=10,  # 최대 모델 저장 개수 설정
-    warmup_steps=500,  # 워밍업 스텝 설정
+    save_steps=1000,  
+    save_total_limit=10,  
+    warmup_steps=500,  
     weight_decay=0.01
 )
 
@@ -123,10 +110,5 @@ trainer = Trainer(
     compute_metrics=compute_metrics
 )
 
-torch.cuda.empty_cache()
-gc.collect()
-
-trainer.train(resume_from_checkpoint = True if get_latest_checkpoint(f'{LOCATION}/model/') else False)
-
-
+trainer.train()
 trainer.save_model(f"{LOCATION}/model")
