ffe21f8328c1f40ec0794749e8548ca6cf69cd5b
20203153 <123941587+20203153@users.noreply.github.com>
2024-04-09 09:13:38 +0900

    model changed kogpt2

--- learn.py
+++ learn.py
@@ -1,5 +1,5 @@
 from torch.utils.data import Dataset, DataLoader
-from transformers import BertTokenizerFast
+from transformers import PreTrainedTokenizerFast
 
 import pandas as pd
 import torch
@@ -7,7 +7,9 @@ import torch
 LOCATION = '.'
 
 # GPT-3 모델과 토크나이저 불러오기
-tokenizer = BertTokenizerFast.from_pretrained("kykim/gpt3-kor-small_based_on_gpt2")
+tokenizer = PreTrainedTokenizerFast.from_pretrained("skt/kogpt2-base-v2",
+                                                      bos_token='</s>', eos_token='</s>', unk_token='<unk>',
+                                                      pad_token='<pad>', mask_token='<mask>')
 max_length = 128
 
 # 훈련용 및 검증용 데이터셋 클래스 정의
@@ -21,8 +23,8 @@ class TextDataset(Dataset):
         return len(self.data)
 
     def __getitem__(self, idx):
-        input_text = str(self.data.iloc[idx]['text'])
-        output_text = str(self.data.iloc[idx]['corrected'])
+        input_text = f'</s>{str(self.data.iloc[idx]['text'])}</s>'
+        output_text = f'</s>{str(self.data.iloc[idx]['corrected'])}</s>'
         input_ids = self.tokenizer.encode(input_text, max_length=self.max_length, truncation=True, padding='max_length')
         output_ids = self.tokenizer.encode(output_text, max_length=self.max_length, truncation=True, padding='max_length')
         return {
@@ -53,13 +55,8 @@ if torch.backends.mps.is_available():
 elif torch.cuda.is_available():
     device = torch.device('cuda')
 
-model = GPT2LMHeadModel.from_pretrained("kykim/gpt3-kor-small_based_on_gpt2")
-
-model.train()
-
-lr = 2e-5
-loss = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+model = GPT2LMHeadModel.from_pretrained("skt/kogpt2-base-v2")
+lr=2e-5
 SAVE_PATH = f"{LOCATION}/all_metrics.pkl"
 
 if os.path.exists(SAVE_PATH):
@@ -81,7 +78,7 @@ def compute_metrics(eval_pred):
     decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
     decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
 
-    bleu_score = metric_bleu.compute(predictions=decoded_preds, references=[decoded_labels])
+    bleu_score = metric_bleu.compute(predictions=decoded_preds, references=[[label] for label in decoded_labels])
 
     return {"bleu": bleu_score["score"]}
 
@@ -90,11 +87,11 @@ data_collactor = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
 training_args = TrainingArguments(
     output_dir=f"{LOCATION}/model", # 모델 저장 경로
     overwrite_output_dir=True,  # 기존 모델을 덮어쓰기
-    learning_rate=2e-5,  # 학습률 설정
+    learning_rate=lr,  # 학습률 설정
     num_train_epochs=5,  # 학습 에포크 설정
     evaluation_strategy="epoch",  # 평가 스트레티지 설정
     per_device_train_batch_size=16,  # 배치 크기 설정
-    per_device_eval_batch_size=16,
+    per_device_eval_batch_size=1,
     save_steps=1000,  # 모델 저장 스텝 설정
     save_total_limit=10,  # 최대 모델 저장 개수 설정
     warmup_steps=500,  # 워밍업 스텝 설정
@@ -112,4 +109,4 @@ trainer = Trainer(
 )
 
 trainer.train(resume_from_checkpoint = True if get_latest_checkpoint(f'{LOCATION}/model/') else False)
-trainer.save_model(f"{LOCATION}/gpt3")
+trainer.save_model(f"{LOCATION}/gpt2")
